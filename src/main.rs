a7a0d4e38246b75f77d9e71bb88edaa9a3cdd58a
Hazel Rella <hazelrella11@gmail.com>
2023-03-29 06:21:36 -0700

    par_iter for speed

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use anyhow::anyhow;
 use clap::Parser;
 use image::ImageFormat;
 use limg::Arguments;
+use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 
 pub fn convert<Q: AsRef<Path>>(path: Q, format: &str) -> Result<PathBuf, anyhow::Error> {
     let mut p = path.as_ref().to_path_buf();
@@ -26,24 +27,27 @@ fn main() -> Result<(), anyhow::Error> {
         return Err(anyhow!("Could not determine format from extension: {:?}", args.target_format));
     }
 
-    for image in glob::glob(&args.input)? {
-        match image {
-            Ok(path) => {
-                match convert(&path, &args.target_format) {
-                    Ok(output) => {
-                        println!("{path:?} -> {output:?}");
-                        if args.delete_after {
-                            if let Err(e) = fs::remove_file(&path) {
-                                println!("Error removing {path:?}: {e:?}");
+    glob::glob(&args.input)?
+        .collect::<Vec<Result<PathBuf, _>>>()
+        .into_par_iter()
+        .for_each(|image| {
+            match image {
+                Ok(path) => {
+                    match convert(&path, &args.target_format) {
+                        Ok(output) => {
+                            println!("{path:?} -> {output:?}");
+                            if args.delete_after {
+                                if let Err(e) = fs::remove_file(&path) {
+                                    println!("Error removing {path:?}: {e:?}");
+                                }
                             }
-                        }
-                    },
-                    Err(err) => println!("{path:?}: {err}"),
-                }
-            },
-            Err(e) => println!("Failed to read with error: {e:?}"),
-        }
-    }
+                        },
+                        Err(err) => println!("{path:?}: {err}"),
+                    }
+                },
+                Err(e) => println!("Failed to read with error: {e:?}"),
+            }
+        });
 
     Ok(())
 }
